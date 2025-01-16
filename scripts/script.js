7babd4b18235f012456dde3fca6abb10200d1907
Matthijs217 <matthijstenbrink@gmail.com>
2025-01-16 16:50:32 +0100

    begin interactie #10

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,12 @@ const dialog = document.querySelector('dialog');
 const showButton = document.querySelector('.showPreference');
 const closeButton = document.querySelector('dialog .closeDialog');
 
+const lightmode = document.querySelector('.lightmode');
+const darkmode = document.querySelector('.darkmode');
+const blackonwhite = document.querySelector('.blackonwhite');
+const whiteonblack = document.querySelector('.whiteonblack');
+
+
 showButton.addEventListener('click', () => {
   dialog.showModal();
   dialog.classList.add('visible');
@@ -12,9 +18,36 @@ closeButton.addEventListener('click', () => {
   dialog.classList.remove('visible');
 });
 
+// sluit als je ernaast klikt
 document.body.addEventListener('click', (event) => {
   if (event.target === dialog) {
     dialog.close();
     dialog.classList.remove('visible');
   }
 });
+
+// Modes switchen
+
+lightmode.addEventListener('click', () => {
+  document.body.classList.add('lightmode');
+  document.body.classList.add('whiteonblack');
+});
+
+darkmode.addEventListener('click', () => {
+  document.body.classList.remove('lightmode');
+  document.body.classList.remove('whiteonblack');
+});
+
+blackonwhite.addEventListener('click', () => {  
+  document.body.classList.remove('lightmode');
+  document.body.classList.remove('whiteonblack');
+});
+
+whiteonblack.addEventListener('click', () => {      
+  document.body.classList.add('lightmode');
+  document.body.classList.add('whiteonblack');
+});
+
+
+
+
